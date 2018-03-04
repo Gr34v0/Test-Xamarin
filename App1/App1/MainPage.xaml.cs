7ae816c9c6fc4242b7463041a50c4cf18c95549e
Gr34v0 <gr34v0@gmail.com>
2018-03-04 13:34:00 -0500

    New page for thing.

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -9,9 +9,31 @@ namespace App1
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+
+        public MainPage()
 		{
 			InitializeComponent();
 		}
+
+        private async void HelloButton_OnClicked(object sender, EventArgs e)
+        {
+            var name = textNameEntry.Text;
+            await DisplayAlert("Greetings", $"Hello {name}!", "Howdy");
+
+        }
+
+        private async void Page1_OnClicked(object sender, EventArgs e)
+        {
+            if ( textNameEntry.Text == "Test User" ) {
+                await Navigation.PushAsync(new Page1());
+            }
+            else
+            {
+                await DisplayAlert("ERROR", $"Username not valid\n" + $"Try again with \"Test User\"", "Okay");
+            }
+          
+        }
+        
+
 	}
 }
